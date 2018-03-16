30bfaa0c45a9e9736e906e778b75fb4c21c9afb5
Chris Schweda <cschweda@gmail.com>
2018-03-16 16:46:51 -0500

    Adjust wordpress docker install

--- wordpress-docker-install/backup.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker exec wordpressdockerinstall_db_1 /usr/bin/mysqldump -u root --password=wordpress wordpress > backup/mysqldb.sql && gzip backup/mysqldb.sql
--- wordpress-docker-install/restore.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-gunzip backup/mysqldb.sql.gz && cat backup/mysqldb.sql | docker exec -i wordpressdockerinstall_db_1 /usr/bin/mysql -u root --password=wordpress wordpress && gzip backup/mysqldb.sql
